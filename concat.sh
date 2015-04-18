3c72cf4e7a1341f1913243b470999825bc604521
Yasuhiro Inami <inamiy@gmail.com>
2015-04-18 14:34:16 +0900

    Move concat.sh to Script directory.

--- concat.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#cat DebugLog/*.swift Vendor/DDFileReader/DDFileReader/*.swift
-awk 'FNR==1{print ""}{print}' DebugLog/*.swift Vendor/DDFileReader/DDFileReader/*.swift > DebugLog.all.swift
